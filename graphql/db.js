8d2cafdcb852f7e98b91387bb96f17e5c217cc73
Ian0714 <54196835+Ian0714@users.noreply.github.com>
2020-02-14 10:12:08 +0900

    deleteMutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,21 +1,21 @@
   let movies = [
     {
-      id: "0",
+      id: 0,
       name: "Star Wars - The new one",
       score: 1
     },
     {
-      id: "1",
+      id: 1,
       name: "Avengers - The new one",
       score: 2  
     },
     {
-      id: "2",
+      id: 2,
       name: "The Godfather I",
       score: 99
     },
     {
-      id: "3",
+      id: 3,
       name: "Logan",
       score: 8
     }
@@ -24,14 +24,14 @@
   export const getMovies = () => movies;
 
   export const getById = id => {
-      const filteredMovies = movies.filter(movie => movie.id === String(id));
+      const filteredMovies = movies.filter(movie => movie.id === id);
       return filteredMovies[0];
   };
 
-  export const deleteMovies = id => {
-    const cleanedMovies = movie.filter(movie => movie.id !== String(id));
+  export const deleteMovie = id => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
     if(movies.length > cleanedMovies.length){
-      movie = cleanedMovies;
+      movies = cleanedMovies;
       return true
     } else {
       return false
